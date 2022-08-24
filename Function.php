361fd26479e8c3448b7224bc8fb481b1d7083db4
iffalimaturrohmah <109191221+iffalimaturrohmah@users.noreply.github.com>
2022-08-24 15:19:39 +0700

    Add files via upload

---
+++ Function.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Function</title>
+</head>
+<body>
+    <?php
+    function jumlah ($a,) {
+        return ($a + $b);
+    }
+
+    echo jumlah (2, 3) ;
+    ?>
+    </body>
+    </html>
+
